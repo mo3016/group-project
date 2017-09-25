3359185fbd2aa5212e1ae3cf93981ec5801a09f5
mo3016 <31695169+mo3016@users.noreply.github.com>
2017-09-25 10:42:53 +0100

    Files for calculations

---
+++ assets.R
@@ -0,0 +1,9 @@
+#File for asset calculations
+#Packages
+install.packages("tidyverse")
+install.packages("ggthemes")
+
+#libraries
+library(ggplot2)
+library(dplyr)
+library(ggthemes)
---
+++ drivers.R
@@ -0,0 +1,9 @@
+#File for drivers calculations
+#Packages
+install.packages("tidyverse")
+install.packages("ggthemes")
+
+#libraries
+library(ggplot2)
+library(dplyr)
+library(ggthemes)
