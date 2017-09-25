9042db5309146d4f10318fe7b6483b5dc9f3da6e
mo3016 <31695169+mo3016@users.noreply.github.com>
2017-09-25 10:20:19 +0100

    Base files

---
+++ Base.R
@@ -0,0 +1,48 @@
+#Packages
+install.packages("tidyverse")
+install.packages("ggthemes")
+
+#libraries
+library(ggplot2)
+library(dplyr)
+library(ggthemes)
+
+#change data (imported manually) into tbl
+data <-tbl_df(bitcoin_dataset)
+
+#Covert date string to Date type
+data <-mutate(data,Date =as.Date(Date,format='%m/%d/%Y' ))
+
+
+#Plot theme ---- use '+theme_publicaton' with ggplot call
+theme_Publication <- function(base_size=14, base_family="sans") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(face = "bold",
+                                      size = rel(1.2), hjust = 0.5),
+            text = element_text(),
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            panel.border = element_rect(colour = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.title.x = element_text(vjust = -0.2),
+            axis.text = element_text(), 
+            axis.line.x = element_line(colour="black"),
+            axis.line.y = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(colour = NA),
+            legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.key.size= unit(0.2, "cm"),
+            
+            legend.title = element_text(face="italic"),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text(face="bold")
+    ))
+  
+}
