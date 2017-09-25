0bf8f7548f1b1e4d753f27b83f176d6d11b9f72c
yiting-wang <32194559+yiting-wang@users.noreply.github.com>
2017-09-25 20:09:57 +0100

    currency.R
    add bitcoin tbl and make a test ggplot

--- currency.R
+++ currency.R
@@ -7,3 +7,15 @@ install.packages("ggthemes")
 library(ggplot2)
 library(dplyr)
 library(ggthemes)
+
+#read csv - remember to change wd using setwd()
+bitcoin_dataset <- read.csv("bitcoin_dataset.csv")
+
+#change data (imported manually) into tbl
+bitcoin <- tbl_df(bitcoin_dataset)
+
+#Covert date string to Date type
+bitcoin <- mutate(bitcoin, Date = as.Date(Date,format = '%m/%d/%Y'))
+
+#a ggplot test
+ggplot(data, aes(x = Date, y = btc_market_price)) + geom_line()
