eb0e666bf53437002fc88ffd529a2e3940c48332
rm100 <rsm216@ic.ac.uk>
2017-09-27 22:37:16 +0100

    Made changes to variable names and added ggplot chart on predicted vs. actual

--- currency.R
+++ currency.R
@@ -18,90 +18,81 @@ bitcoin <- tbl_df(bitcoin_dataset)
 bitcoin <- mutate(bitcoin, Date = as.Date(Date,format = '%m/%d/%Y'))
 View(bitcoin)
 
-#calculate return on price
+#Calculate new variables for analysis, add this to bitcoin dataframe, 1. price return  called "btc_price_return" and 2. coin supply daily as "btc_daily_new_coins"
+#New variables are calculated and added to the "bitcoin" dataframe 
 
-bitcoin <- mutate(bitcoin, return=(btc_market_price-lag(btc_market_price))/lag(btc_market_price)*100, daily_new_coins=btc_total_bitcoins-lag(btc_total_bitcoins))
+bitcoin <- mutate(bitcoin, btc_price_return=(btc_market_price-lag(btc_market_price))/lag(btc_market_price)*100, btc_daily_new_coins=btc_total_bitcoins-lag(btc_total_bitcoins))
 
-View(bitcoin)
+#View structure of new dataframe with new variables
+str(bitcoin)
 
-tail(bitcoin$return, n= 50)
-
-#extract all useful columns for currency analysis
-bitcoin_currency <- bitcoin %>% 
-  select("Date", "btc_market_price", "return", "btc_total_bitcoins", "daily_new_coins", "btc_n_transactions",
+#Extract all useful columns from "bitcoin" dataframe for data analysis, place these new columns and with selected date periods into a dataframe called "bitcoin_variables"
+bitcoin_variables <- bitcoin %>% 
+  select("Date", "btc_market_price", "btc_price_return", "btc_total_bitcoins", "btc_daily_new_coins", "btc_n_transactions",
          "btc_n_transactions_total", "btc_n_transactions_excluding_popular", "btc_n_transactions_per_block",
          "btc_n_transactions_excluding_chains_longer_than_100", "btc_market_cap", 
          "btc_trade_volume", "btc_median_confirmation_time", "btc_hash_rate", "btc_output_volume",
          "btc_estimated_transaction_volume", "btc_estimated_transaction_volume_usd")%>%
   filter(Date >= "2013-04-02")
 
-View(bitcoin_currency)
+#View the structure of the new dataframe "bitcoin_variables"
+str(bitcoin_variables) 
 
-#simple plots
-ggplot(bitcoin_currency, aes(x = Date, y = btc_market_price)) + geom_line() 
-ggplot(bitcoin_currency, aes(x = Date, y = btc_total_bitcoins)) + geom_line() 
-ggplot(bitcoin_currency, aes(x = Date, y = btc_n_transactions_total)) + geom_line() 
+#Plot potential variables for analysis in "bitcoin_variables", this will inform us of the variables' current functional form
+ggplot(bitcoin_variables, aes(x = Date, y = btc_market_price)) + geom_line()
+ggplot(bitcoin_variables, aes(x = Date, y = btc_total_bitcoins)) + geom_line() 
+ggplot(bitcoin_variables, aes(x = Date, y = btc_daily_new_coins)) + geom_line() 
+ggplot(bitcoin_variables, aes(x = Date, y = btc_n_transactions)) + geom_line() 
+ggplot(bitcoin_variables, aes(x = Date, y = btc_n_transactions_total)) + geom_line() 
 
+#Tranform key variables in "bitcoin_variables" to natural logarithms i.e. to linearise our variables for modelling
+log_btc_market_price <- log (bitcoin_variables$btc_market_price)
+log_btc_total_bitcoins <- log (bitcoin_variables$btc_total_bitcoins)
+log_btc_daily_new_coins <- log (bitcoin_variables$btc_daily_new_coins)
+log_btc_n_transactions <- log (bitcoin_variables$btc_n_transactions)
+log_btc_n_transactions_total <- log (bitcoin_variables$btc_n_transactions_total)
 
-#convert varibles to logs
-
-log_btc_market_price <- log (bitcoin_currency$btc_market_price)
-log_return <- log (bitcoin_currency$return)
-log_btc_total_bitcoins <- log (bitcoin_currency$btc_total_bitcoins)
-log_daily_new_coins <- log (bitcoin_currency$daily_new_coins)
-log_btc_trade_volume <- log (bitcoin_currency$btc_trade_volume)
-log_btc_n_transactions <- log (bitcoin_currency$btc_n_transactions)
-log_btc_n_transactions_total <- log (bitcoin_currency$btc_n_transactions_total)
-
-
-#new dataframe with log variables
-
-log_var <- data.frame(bitcoin_currency$Date,log_btc_market_price,bitcoin_currency$return,log_btc_total_bitcoins,log_daily_new_coins,log_btc_trade_volume,log_btc_n_transactions,log_btc_n_transactions_total)
-
-
-#add dummy variable for new_coins
-log_var <- mutate(log_var, dummy = 0)
-
-for (i in 599:nrow(log_var)){
-  log_var[i, "dummy"] <- 1 
-}
-
-View(log_var)
+#Log variables are now added to a new dataframe called "bitcoin_variables_log" which can be used as the basis for regression
+bitcoin_variables_log <- data.frame(bitcoin_variables$Date,log_btc_market_price,bitcoin_variables$btc_price_return,log_btc_total_bitcoins,log_btc_daily_new_coins,log_btc_n_transactions,log_btc_n_transactions_total)
 
 #Plots of log variables
-ggplot(log_var, aes(x = bitcoin_currency$Date, y = log_btc_market_price)) + geom_line() 
-ggplot(log_var, aes(x = bitcoin_currency$Date, y = bitcoin_currency$return)) + geom_line() 
-ggplot(log_var, aes(x = bitcoin_currency$Date, y = log_btc_total_bitcoins)) + geom_line() 
-ggplot(log_var, aes(x = bitcoin_currency$Date, y = log_daily_new_coins)) + geom_line() 
-ggplot(log_var, aes(x = bitcoin_currency$Date, y = log_btc_trade_volume)) + geom_line() 
-ggplot(log_var, aes(x = bitcoin_currency$Date, y = log_btc_n_transactions)) + geom_line() 
-ggplot(log_var, aes(x = bitcoin_currency$Date, y = log_btc_n_transactions_total)) + geom_line() 
+ggplot(bitcoin_variables_log, aes(x = bitcoin_variables$Date, y = log_btc_market_price)) + geom_line() 
+ggplot(bitcoin_variables_log, aes(x = bitcoin_variables$Date, y = log_btc_total_bitcoins)) + geom_line() 
+ggplot(bitcoin_variables_log, aes(x = bitcoin_variables$Date, y = log_btc_daily_new_coins)) + geom_line() 
+ggplot(bitcoin_variables_log, aes(x = bitcoin_variables$Date, y = log_btc_n_transactions)) + geom_line() 
+ggplot(bitcoin_variables_log, aes(x = bitcoin_variables$Date, y = log_btc_n_transactions_total)) + geom_line() 
 
-#multiple regression
-reg_1 <- lm(log_btc_market_price ~ log_btc_n_transactions_total + log_btc_total_bitcoins, data=log_var)
+#Notice in plot below, structural break in daily coin supply at 11/07/2017 and after. Bitcoin officially restricted new daily coin supply by halve. 
+log_btc_daily_new_coins <- log (bitcoin_variables$btc_daily_new_coins)
+
+#Below adds a dummy variable to account for this, where dummy = 1 on dates 11/07/2016 and beyond
+bitcoin_variables_log <- mutate(bitcoin_variables_log, dummy = 0)
+
+for (i in 599:nrow(bitcoin_variables_log)){
+  bitcoin_variables_log[i, "dummy"] <- 1 
+}
+
+View(bitcoin_variables_log)
+
+#Multiple regressions
+#Regression 1 (reg_1) market price on transactions and total bitcoin supply with intercept
+reg_1 <- lm(log_btc_market_price ~ log_btc_n_transactions_total + log_btc_total_bitcoins, data=bitcoin_variables_log)
 summary(reg_1)
 
 reg_1.res <- residuals(reg_1)
 reg_1.predict <- predict(reg_1)
 
-reg_1.res
-reg_1.predict
-log_btc_market_price
+#log_btc_market_price
 
-comparison <- data.frame(bitcoin_currency$Date,log_btc_market_price,reg_1.predict,reg_1.res)
+comparison <- data.frame(bitcoin_variables$Date,log_btc_market_price,reg_1.predict,reg_1.res)
 
-head(comparison,n=10)
+comparison_2 <- comparison %>%
+  filter(bitcoin_variables.Date >= "2015-01-02")
 
-ggplot(comparison, aes(x=comparison$bitcoin_currency.Date,y=log_btc_market_price))+geom_point() + geom_point(aes(y = reg_1.predict, color=reg_1.predict), shape = 1)
+View(comparison_2)
 
+ggplot(comparison_2, aes(x=bitcoin_variables.Date,y=log_btc_market_price))+geom_point(colour="black", shape=21, fill="orange")+geom_point(aes(y=reg_1.predict),colour="Red",shape=22,size=-1)
 
-reg_2 <- lm(log_btc_market_price ~ log_btc_n_transactions_total + log_btc_total_bitcoins + dummy, data=log_var)
+#Regression 2 (reg_2) market price on transactions and total bitcoin supply with intercept and dummy
+reg_2 <- lm(log_btc_market_price ~ log_btc_n_transactions_total + log_btc_total_bitcoins + dummy, data=bitcoin_variables_log)
 summary(reg_2)
-
-reg_3 <- lm(log_btc_market_price ~ log_btc_n_transactions + log_daily_new_coins, data=log_var)
-summary(reg_3)
-
-reg_4 <- lm(log_btc_market_price ~ log_btc_n_transactions + log_daily_new_coins + dummy, data=log_var)
-summary(reg_4)
-
-#Explanation: btc_market_price = 9.246e+01 + (-1.902e-05) * btc_total_bitcoins + 8.751e-06  * btc_n_transactions_total
