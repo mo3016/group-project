deeef9d3c804b13c98a684116b1a0fc730c70216
thamster1 <32263801+thamster1@users.noreply.github.com>
2017-09-27 18:05:55 +0100

    Add files via upload
    First commit on Assets section (or now comparison to other currencies).
    Requires Mark's and Letty's section to be run first.

--- assets.R
+++ assets.R
@@ -1,4 +1,7 @@
 #File for asset calculations
+#requires Mark's section to be run first to get the dataset imported
+
+
 #Packages
 install.packages("tidyverse")
 install.packages("ggthemes")
@@ -7,3 +10,86 @@ install.packages("ggthemes")
 library(ggplot2)
 library(dplyr)
 library(ggthemes)
+library(readr)
+library(lubridate)
+library(tidyr)
+
+
+#Importing FX CSV and removing repeated date column
+FX <- read_csv("FX.csv")
+fx <- select(FX, Date, CNYUSD, JPYUSD, GBPUSD, EURUSD)
+
+#Importing Gold Dataset
+gold <- read_csv("Gold Series Data.csv")
+names(gold)[2] <- 'goldprice'
+
+#Formatting Date column, then merging datasets, removing NA observations and selecting required variables
+fx$Date <- dmy(fx$Date)
+gold$Date <- dmy(gold$Date)
+fxdata <- btc_data %>% left_join(fx, by='Date') %>% left_join(gold, by='Date')
+fxdata <- fxdata %>% select(Date, btc_market_price, JPYUSD, CNYUSD, EURUSD, GBPUSD, goldprice) %>% filter(!is.na(JPYUSD))
+
+#Defining a function to calculate returns and using it to create columns for returns
+#Returns have to be recalculated as some prices have been removed
+gen_ret <- function(colname) {
+  (colname - lag(colname)) / lag(colname) * 100
+}
+fxdata <- mutate(fxdata, btcret = gen_ret(btc_market_price), cnyret = gen_ret(CNYUSD), eurret = gen_ret(EURUSD), jpyret = gen_ret(JPYUSD), gbpret = gen_ret(GBPUSD), goldret = gen_ret(goldprice))
+
+
+#Correlation table
+fxdata %>% select(btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% cor(use = 'complete.obs')
+
+#New Long dataset and Scatter against BTC: Note the lack of relationship in all the charts
+fxdata3 <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% gather(curr, ret, -Date, -btcret)
+ggplot(fxdata3, aes(x = btcret)) + geom_point(aes(y=ret)) + facet_wrap(~curr) + xlim(-5, 5) + ylim(-5, 5)
+rm(fxdata3)
+
+#New Long dataset and Scatter against EUR: Note the relationships between EURJPY and GBPEUR pairs. 
+#CNY has a somewhat insignificant relationship, possibly explained by Chinese currency controls. Also explains extremely low volatility
+fxdata3a <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% gather(curr, ret, -Date, -eurret)
+ggplot(fxdata3a, aes(x = eurret)) + geom_point(aes(y=ret)) + facet_wrap(~curr) + xlim(-5, 5) + ylim(-5, 5)
+rm(fxdata3a)
+
+#Scatterplot of JPY vs Gold and BTC: JPY has positive relationship with Gold that should be visible in BTC if it was a safe haven currency
+fxdata3b <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% gather(curr, ret, -Date, -jpyret)
+ggplot(fxdata3b, aes(x = jpyret)) + geom_point(aes(y=ret)) + facet_wrap(~curr) + xlim(-5, 5) + ylim(-5, 5)
+rm(fxdata3b)
+
+
+#boxplot of returns (Can we draw a line across boxplot at mean of btcret?)
+fxdata3a <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% gather(curr, ret, -Date)
+ggplot(fxdata3a, aes(x=curr)) + geom_boxplot(aes(y=ret)) + ylim(-5, 5)
+rm(fxdata3a)
+
+#Density plots of returns (Left out CNY as its variance is too small)
+fxdata3b <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, goldret) %>% gather(curr, ret, -Date)
+ggplot(fxdata3b, aes(x = ret)) + geom_density(aes(colour = curr)) + xlim(-7.5, 7.5) 
+rm(fxdata3b)
+
+
+
+#Testing of means
+t.test(fxdata2$gbpret, fxdata2$eurret)
+t.test(fxdata2$gbpret, fxdata2$jpyret)
+t.test(fxdata2$eurret, fxdata2$jpyret)
+t.test(fxdata2$cnyret, fxdata2$jpyret)
+
+t.test(fxdata2$btcret, fxdata2$eurret)
+t.test(fxdata2$btcret, fxdata2$jpyret)
+t.test(fxdata2$btcret, fxdata2$gbpret)
+t.test(fxdata2$btcret, fxdata2$cnyret)
+t.test(fxdata2$btcret, fxdata2$goldret)
+
+#Similarly do a var.test(): However note that even between currencies, variance equality tests would show variances are not equal
+#Thus this test may not mean very much
+var.test(fxdata2$btcret, fxdata2$eurret)
+var.test(fxdata2$btcret, fxdata2$jpyret)
+var.test(fxdata2$btcret, fxdata2$gbpret)
+var.test(fxdata2$btcret, fxdata2$cnyret)
+var.test(fxdata2$btcret, fxdata2$goldret)
+
+
+
+
+
