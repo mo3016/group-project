67d947e737774cd412ec6992b6bd106b723aacbc
thamster1 <32263801+thamster1@users.noreply.github.com>
2017-09-29 00:48:19 +0100

    -CNY, +Standardised Returns
    Removed CNY from all plots and data, as it was not adding much to our argument and had to be repeatedly explained as being 'different' due to its nature as a managed currency
    Added the code for the standardised returns density plot
    Reordered the code according to GDocs appearance

--- assets.R
+++ assets.R
@@ -13,6 +13,7 @@ library(ggthemes)
 library(readr)
 library(lubridate)
 library(tidyr)
+library(reshape2)
 
 
 #Importing FX CSV and removing repeated date column
@@ -27,19 +28,18 @@ names(gold)[2] <- 'goldprice'
 fx$Date <- dmy(fx$Date)
 gold$Date <- dmy(gold$Date)
 fxdata <- btc_data %>% left_join(fx, by='Date') %>% left_join(gold, by='Date')
-fxdata <- fxdata %>% select(Date, btc_market_price, JPYUSD, CNYUSD, EURUSD, GBPUSD, goldprice) %>% filter(!is.na(JPYUSD))
+fxdata <- fxdata %>% select(Date, btc_market_price, JPYUSD, EURUSD, GBPUSD, goldprice) %>% filter(!is.na(JPYUSD))
 
 #Defining a function to calculate returns and using it to create columns for returns
 #Returns have to be recalculated as some prices have been removed
 gen_ret <- function(colname) {
   (colname - lag(colname)) / lag(colname) * 100
 }
-fxdata <- mutate(fxdata, btcret = gen_ret(btc_market_price), cnyret = gen_ret(CNYUSD), eurret = gen_ret(EURUSD), jpyret = gen_ret(JPYUSD), gbpret = gen_ret(GBPUSD), goldret = gen_ret(goldprice))
+fxdata <- mutate(fxdata, btcret = gen_ret(btc_market_price), eurret = gen_ret(EURUSD), jpyret = gen_ret(JPYUSD), gbpret = gen_ret(GBPUSD), goldret = gen_ret(goldprice))
 
 
 #Correlation table
-corr_table<-fxdata %>% select(btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% cor(use = 'complete.obs')
-library(reshape2)
+corr_table<-fxdata %>% select(btcret, gbpret, jpyret, eurret, goldret) %>% cor(use = 'complete.obs')
 melted_currency <- melt(corr_table)
 
 ggplot(data = melted_currency, aes(x=Var1, y=Var2, fill=value)) + 
@@ -48,48 +48,27 @@ ggplot(data = melted_currency, aes(x=Var1, y=Var2, fill=value)) +
   xlab( "") +ggtitle('Correlation Heatmap') 
 
 #New Long dataset and Scatter against BTC: Note the lack of relationship in all the charts
-fxdata3 <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% gather(curr, ret, -Date, -btcret)
-ggplot(fxdata3, aes(x = btcret)) + geom_point(aes(y=ret)) + facet_wrap(~curr) + xlim(-5, 5) + ylim(-5, 5) +
-theme_Publication() +
+fxdata3 <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, goldret) %>% gather(curr, ret, -Date, -btcret)
+ggplot(fxdata3, aes(x = btcret)) + geom_point(aes(y=ret)) + facet_grid(~curr) + xlim(-5, 5) + ylim(-5, 5) +
+  theme_Publication() +
   ylab( "Returns") + xlab( "Bitcoin Returns") +ggtitle('Bitcoin returns vs other currencies') 
 rm(fxdata3)
 
 #New Long dataset and Scatter against EUR: Note the relationships between EURJPY and GBPEUR pairs. 
 #CNY has a somewhat insignificant relationship, possibly explained by Chinese currency controls. Also explains extremely low volatility
-fxdata3a <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% gather(curr, ret, -Date, -eurret)
-ggplot(fxdata3a, aes(x = eurret)) + geom_point(aes(y=ret)) + facet_wrap(~curr) + xlim(-5, 5) + ylim(-5, 5) +
+fxdata3a <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, goldret) %>% gather(curr, ret, -Date, -eurret)
+ggplot(fxdata3a, aes(x = eurret)) + geom_point(aes(y=ret)) + facet_grid(~curr) + xlim(-5, 5) + ylim(-5, 5) +
   theme_Publication() +
   ylab( "Returns") + xlab( "Euro Returns") +ggtitle('Euro returns vs other currencies') 
 rm(fxdata3a)
 
 #Scatterplot of JPY vs Gold and BTC: JPY has positive relationship with Gold that should be visible in BTC if it was a safe haven currency
-fxdata3b <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, cnyret, goldret) %>% gather(curr, ret, -Date, -jpyret)
-ggplot(fxdata3b, aes(x = jpyret)) + geom_point(aes(y=ret)) + facet_wrap(~curr) + xlim(-5, 5) + ylim(-5, 5) +
+fxdata3b <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, goldret) %>% gather(curr, ret, -Date, -jpyret)
+ggplot(fxdata3b, aes(x = jpyret)) + geom_point(aes(y=ret)) + facet_grid(~curr) + xlim(-5, 5) + ylim(-5, 5) +
   theme_Publication() +
   ylab( "Returns") + xlab( "JPY Returns") +ggtitle('JPY returns vs other currencies') 
 rm(fxdata3b)
 
-
-
-
-#Density plots of returns (Left out CNY as its variance is too small)
-fxdata3b <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, goldret) %>% gather(curr, ret, -Date)
-ggplot(fxdata3b, aes(x = ret)) + geom_density(aes(colour = curr)) + xlim(-7.5, 7.5) +
-  ylab( "Returns") + xlab( "Currency") +ggtitle('Density of returns')+
-  theme_Publication()
-rm(fxdata3b)
-
-
-#Calculate t-test on Hypothesis: mean = 0 
-mean0 <- function(colname) {
-  round(as.numeric(t.test(colname)[3]), 3)
-}
-pvmean0 <- c('BTC' = mean0(fxdata$btcret),
-             'EUR' = mean0(fxdata$eurret),
-             'GBP' = mean0(fxdata$gbpret),
-             'JPY' = mean0(fxdata$jpyret),
-             'CNY' = mean0(fxdata$cnyret),
-             'Gold' = mean0(fxdata$goldret))
 
 #Calculate t-test on Hypothesis: mean1 = mean2 
 meaneq <- function(colname) {
@@ -100,8 +79,50 @@ pvmeaneq <- c('BTC' = meaneq(fxdata$btcret),
               'EUR' = meaneq(fxdata$eurret),
               'GBP' = meaneq(fxdata$gbpret),
               'JPY' = meaneq(fxdata$jpyret),
-              'CNY' = meaneq(fxdata$cnyret),
               'Gold' = meaneq(fxdata$goldret))
+pvmeaneq
+
+
+#Calculate t-test on Hypothesis: mean = 0 
+mean0 <- function(colname) {
+  round(as.numeric(t.test(colname)[3]), 3)
+}
+pvmean0 <- c('BTC' = mean0(fxdata$btcret),
+             'EUR' = mean0(fxdata$eurret),
+             'GBP' = mean0(fxdata$gbpret),
+             'JPY' = mean0(fxdata$jpyret),
+             'Gold' = mean0(fxdata$goldret))
+pvmean0
+
+
+
+#Density plots of returns (Left out CNY as its variance is too small)
+fxdata3b <- fxdata %>% select(Date, btcret, gbpret, jpyret, eurret, goldret) %>% gather(curr, ret, -Date)
+ggplot(fxdata3b, aes(x = ret)) + geom_density(aes(colour = curr)) + xlim(-7.5, 7.5) +
+  ylab( "Density") + xlab( "Returns") +ggtitle('Density of returns')+
+  theme_Publication()
+rm(fxdata3b)
+
+
+#Creating function to standardise returns
+stand <- function(input) {
+  a <- (input - mean(input, na.rm = T)) / sd(input, na.rm = T)
+  return (a)
+}
+
+#Creating dataframe of standardised returns and plotting
+stdretdf <- data.frame('btc' = stand(fxdata$btcret), 
+                       'eur' = stand(fxdata$eurret), 
+                       'gbp' = stand(fxdata$gbpret), 
+                       'jpy' = stand(fxdata$jpyret), 
+                       'gold' = stand(fxdata$goldret))
+
+stdretdf2 <- gather(stdretdf, curr, ret)
+
+ggplot(stdretdf2) + geom_density(aes(x=ret, colour = curr)) + xlim(-3, 3) +
+  ylab( "Density") + xlab( "Standardised Returns") +
+  ggtitle('Density of standardised returns') +
+  theme_Publication()
 
 
 
@@ -112,12 +133,3 @@ ggplot(fxdata3a, aes(x=curr)) + geom_boxplot(aes(y=ret)) + ylim(-5, 5)+ theme_Pu
   theme_Publication()+
   ylab( "Returns") + xlab( "Currency") +ggtitle('Mean of returns')
 rm(fxdata3a)
-
-
-
-
-
-
-
-
-
